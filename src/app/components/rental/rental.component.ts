216698e8856455a5f1b4cc6389637c85d8185225
Ruken Afşin <rukenafsin13@gmail.com>
2023-12-12 00:17:38 +0300

    Rental GetAll Added

--- src/app/components/rental/rental.component.ts
+++ src/app/components/rental/rental.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Rental } from '../../models/rental';
+import { RentalService } from '../../services/rental.service';
 
 @Component({
   selector: 'app-rental',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './rental.component.css'
 })
 export class RentalComponent {
-
-}
+  rentals:Rental[]=[];
+  dataLoaded=false;
+  
+  
+  constructor(private rentalService:RentalService){
+  }
+  ngOnInit(): void {
+     this.getRentals();
+  }
+  
+  getRentals(){
+  this.rentalService.getRentals().subscribe(response=>{
+    this.rentals=response.data
+    this.dataLoaded=true;
+   })
+   } 
+  }
+  
