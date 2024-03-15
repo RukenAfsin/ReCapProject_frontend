c05bb3e1aebc17047a633d953d9f824a424e0fe1
Ruken Afşin <rukenafsin13@gmail.com>
2024-03-15 12:38:21 +0300

    Payment page changed but bug can not fixed yet

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -6,77 +6,59 @@ import { Location } from '@angular/common';
 import {FormGroup, FormBuilder, FormControl,Validators, ValidationErrors} from "@angular/forms"
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
-
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.css'
+  styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent {
-  payments:Payment[]=[];
-  paymentData:Payment;
+  payments: Payment[] = [];
+  paymentData: Payment;
   submitted = false;
   currentStep: number = 1;
 
-  frm:FormGroup;
-  firstFormGroup: FormGroup;
+  // frm: FormGroup;
+  // firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   isLinear = false;
 
+  constructor(private paymentService: PaymentService,
+    private location: Location,
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private spinnerModule: MatProgressSpinnerModule,
+    private _formBuilder: FormBuilder) {
 
 
-  constructor(private paymentService:PaymentService,
-    private location:Location,
-    private formBuilder:FormBuilder,
-    private activatedRoute:ActivatedRoute,
-    private spinnerModule:MatProgressSpinnerModule,
-    private _formBuilder: FormBuilder){
-      const paymentData: Partial<Payment> = {
-        cardOwner: undefined,
-        expiryMonth: undefined,
-        expiryYear: undefined,
-        cvv: undefined,
-      };
-      
-
-    this.frm = this._formBuilder.group({
+    this.secondFormGroup = this.formBuilder.group({
       cardNo: ["", [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
       expiryMonth: ["", [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
       expiryYear: ["", [Validators.required, Validators.minLength(4), Validators.maxLength(4)]],
       cvv: ["", [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
       cardOwner: ["", [Validators.required, Validators.minLength(2), Validators.maxLength(50)]]
     });
-
-    // this.secondFormGroup = this._formBuilder.group({
-    //   secondCtrl: ['', Validators.required]
-    // });
-    }
-
-    get component() {
-      return this.frm.controls;
-    }
-  
-
-
-addPayments(){
-  this.submitted = true;
-  const newPayment:Partial<Payment>={
-    cardOwner:this.firstFormGroup.value.cardOwner,
-    cvv:this.firstFormGroup.value.cvv,
-    expiryMonth:this.firstFormGroup.value.expiryMonth,
-    expiryYear:this.firstFormGroup.value.expiryYear
   }
-  this.paymentService.addPayments(newPayment).subscribe((response)=>{
-    this.payments=response.data;
-    console.log(response)
-  })
-}
 
+  get component() {
+    return this.secondFormGroup.controls;
+  }
 
-
+  addPayments() {
+    this.submitted = true;
+    const newPayment: Partial<Payment> = {
+      cardOwner: this.secondFormGroup.value.cardOwner,
+      cardNo:this.secondFormGroup.value.cardNo,
+      cvv: this.secondFormGroup.value.cvv,
+      expiryMonth: this.secondFormGroup.value.expiryMonth,
+      expiryYear: this.secondFormGroup.value.expiryYear
+    }
+    this.paymentService.addPayments(newPayment).subscribe((response) => {
+      this.payments = response.data;
+      console.log(response)
+    })
+  }
 
   goBack(): void {
     this.location.back();
   }
-  
 }
